d4343e0d6ebb7d4a0c05e39d26432122368927b9
karyakirana <adibpratama2@gmail.com>
2022-01-14 11:29:14 +0700

    prepare datatables

--- app/Http/Controllers/Stock/StockMasukController.php
+++ app/Http/Controllers/Stock/StockMasukController.php
@@ -7,5 +7,33 @@ use Illuminate\Http\Request;
 
 class StockMasukController extends Controller
 {
-    //
+    public function index()
+    {
+        // view daftar stock masuk semua
+    }
+
+    public function datatablesIndex()
+    {
+        //
+    }
+
+    public function indexBaik()
+    {
+        // view daftar stock masuk baik
+    }
+
+    public function datatablesIndexBaik()
+    {
+        //
+    }
+
+    public function indexRusak()
+    {
+        // view daftar stock rusak baik
+    }
+
+    public function datatablesRusak()
+    {
+        //
+    }
 }
--- app/Http/Controllers/Stock/StockOpnameController.php
+++ app/Http/Controllers/Stock/StockOpnameController.php
@@ -7,5 +7,33 @@ use Illuminate\Http\Request;
 
 class StockOpnameController extends Controller
 {
-    //
+    public function index()
+    {
+        // view stock opname semua
+    }
+
+    public function datatablesIndex()
+    {
+        //
+    }
+
+    public function indexBaik()
+    {
+        // view stock opname baik
+    }
+
+    public function datatablesBaik()
+    {
+        //
+    }
+
+    public function indexRusak()
+    {
+        // view stock opname rusak
+    }
+
+    public function datatablesRusak()
+    {
+        //
+    }
 }

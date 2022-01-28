7e8ea80270036e3b0414b85ad35f82bf9b26ac95
luthfanq <chusdiansyahhhh@gmail.com>
2022-01-28 14:40:06 +0700

    dari adib mac

--- resources/views/livewire-tables/rows/penjualan_table.blade.php
+++ resources/views/livewire-tables/rows/penjualan_table.blade.php
@@ -23,10 +23,11 @@
 <x-livewire-tables::bs5.table.cell width="10%" class="text-center">
     @if($row->status_bayar == 'belum')
     <button type="button" class="btn btn-flush btn-active-color-info" wire:click="edit({{$row->id}})"><i class="far fa-edit"></i></button>
-    <button type="button" class="btn btn-flush btn-active-color-danger" wire:click="$emit('showModal', {{$row->id}})"><i class="fas fa-indent"></i></button>
-    <button type="button" class="btn btn-flush btn-active-color-dark" wire:click="print({{$row->id}})" ><i class="fas fa-print"></i></button>
+
     <button type="button" class="btn btn-flush btn-active-color-dark" wire:click="destroy({{$row->id}})" ><i class="fas fa-trash"></i></button>
     @endif
+        <button type="button" class="btn btn-flush btn-active-color-danger" wire:click="$emit('showModal', {{$row->id}})"><i class="fas fa-indent"></i></button>
+        <button type="button" class="btn btn-flush btn-active-color-dark" wire:click="print({{$row->id}})" ><i class="fas fa-print"></i></button>
 </x-livewire-tables::bs5.table.cell>
 
 

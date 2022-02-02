9a446a0da55e07954aed9476b6e2fdd530b4a9c7
karyakirana <adibpratama2@gmail.com>
2022-02-02 17:10:05 +0700

    update di rumah

---
+++ database/migrations/2022_02_01_220534_create_tax_perusahaan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxPerusahaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tax_perusahaan', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->string('nama');
+            $table->text('alamat');
+            $table->string('npwp');
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tax_perusahaan');
+    }
+}
